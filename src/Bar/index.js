23f432604b054cb6b43dd5356ad23fb6078753c7
Salinder Sidhu <salinder.sid@gmail.com>
2021-05-23 00:30:38 -0400

    Added documentation and refactored utils

--- src/Bar/index.js
+++ src/Bar/index.js
@@ -1,9 +1,20 @@
+/**
+ * Copyright (c) Salinder Sidhu and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text } from "react-native";
 
 import Styles from "./styles";
 
+/**
+ * A customizable Bar with text used to draw the ScaleBar.
+ */
 function Bar(props) {
   const { text, size, barStyle, textStyle } = props;
 
@@ -17,9 +28,21 @@ function Bar(props) {
 }
 
 Bar.propTypes = {
+  /**
+   * Size of the bar.
+   */
   size: PropTypes.number.isRequired,
+  /**
+   * Text inside the bar.
+   */
   text: PropTypes.string.isRequired,
+  /**
+   * Style for the Bar.
+   */
   barStyle: PropTypes.object.isRequired,
+  /**
+   * Style for the Bar's text.
+   */
   textStyle: PropTypes.object.isRequired,
 };
 
--- src/styles.js
+++ src/styles.js
@@ -1,9 +1,26 @@
+/**
+ * Copyright (c) Salinder Sidhu and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { StyleSheet } from "react-native";
 
+/**
+ * Styles for ScaleBar.
+ */
 const Styles = StyleSheet.create({
+  /**
+   * Style of ScaleBar container.
+   */
   container: {
     position: "absolute",
   },
+  /**
+   * Default style used for the ScaleBar.
+   */
   defaultBarStyle: {
     borderWidth: 1,
     borderStyle: "solid",
@@ -12,6 +29,9 @@ const Styles = StyleSheet.create({
     borderColor: "rgba(0, 0, 0, 1)",
     backgroundColor: "rgba(255, 255, 255, 0.5)",
   },
+  /**
+   * Default style used for the ScaleBar's text.
+   */
   defaultBarTextStyle: {
     fontSize: 10,
     paddingTop: 5,
